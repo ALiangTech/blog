ace33e49e88fb95416c5b102c736314cf4687762
hanjun <hanjun@yiye.ai>
2023-03-09 14:41:59 +0800

    修改ci

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import setupRouter from './routers'
 import Introduce from './pages/introduce/introduce'
-
+import './App.css'
 function App() {
   return (
     <div className="App grid h-screen w-screen">
-      <div className=''>{Introduce()}</div>
-       {setupRouter()}
+      <div>{Introduce()}</div>
+      <div>{setupRouter()}</div>
     </div>
   )
 }
